73ac340cfede2dc012fc3f8e80756da3f9e11394
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-01-17 18:30:28 -0500

    updata api clients

---
+++ apps/panel-admin/app/login/layout.tsx
@@ -0,0 +1,17 @@
+import { getSession } from '@/lib/session'
+import { redirect } from 'next/navigation'
+import { ReactNode } from 'react'
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  const session = await getSession()
+
+  if (session) {
+    redirect('/dashboard')
+  }
+
+  return <>{children}</>
+}
