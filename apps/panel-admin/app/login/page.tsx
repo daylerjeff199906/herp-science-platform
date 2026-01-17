b6a156d4c3db86231a169be9edadc2152601e4f2
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-01-17 05:16:48 -0500

    add login form

--- apps/panel-admin/app/login/page.tsx
+++ apps/panel-admin/app/login/page.tsx
@@ -4,21 +4,23 @@ import Image from 'next/image'
 
 export default function LoginPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
+    <div className="grid min-h-svh lg:grid-cols-3">
+      <div className="flex flex-col gap-4 p-6 md:p-10 lg:col-span-1">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
-            <LogoRender className="h-6 w-6" />
-            Panel Adminitrativo
+            <LogoRender className="h-16 w-16" />
+            <span className="max-w-48 leading-tight text-xl">
+              Vertebrados IIAP - Admin Panel
+            </span>
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
+          <div className="w-full max-w-sm">
             <LoginForm />
           </div>
         </div>
       </div>
-      <div className="bg-muted relative hidden lg:block">
+      <div className="bg-muted relative hidden lg:block lg:col-span-2">
         <Image
           src="https://vertebrados.iiap.gob.pe/_next/static/media/specie_default.3717d154.webp"
           alt="Image"
--- packages/ui/src/logo.tsx
+++ packages/ui/src/logo.tsx
@@ -1,22 +1,13 @@
 export const LogoRender = ({
   className,
   ...props
-}: React.ComponentProps<'svg'>) => {
+}: React.ComponentProps<'img'>) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+    <img
+      src="https://aguasamazonicas.org/wp-content/uploads/2021/06/imagem_2023-05-29_120804614.png"
+      alt="Logo Vertebrados"
       className={className}
       {...props}
-    >
-      <circle cx="12" cy="12" r="10" />
-      <path d="M12 16v-4" />
-      <path d="M12 8h.01" />
-    </svg>
+    />
   )
 }
