08cf85e09a911825fbc0b7be9d1d847d1fdb9c76
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-01-17 18:48:10 -0500

    updata api clients

---
+++ apps/panel-admin/components/miscellaneous/sidebar-inset.tsx
@@ -0,0 +1,45 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+import { Separator } from '@/components/ui/separator'
+import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar'
+
+interface SidebarInsetProps {
+  children: React.ReactNode
+  currentPage: string
+}
+
+export const SidebarInsetCustom = ({
+  children,
+  currentPage,
+}: SidebarInsetProps) => {
+  return (
+    <SidebarInset>
+      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>{currentPage}</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </header>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+    </SidebarInset>
+  )
+}
