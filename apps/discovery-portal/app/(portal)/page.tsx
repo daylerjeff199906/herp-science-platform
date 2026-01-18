d63829318e3c8e0fd3b5da5c4a69bd657450f76a
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-01-18 13:20:26 -0500

    feat: add header test

--- apps/discovery-portal/app/(portal)/page.tsx
+++ apps/discovery-portal/app/(portal)/page.tsx
@@ -1,24 +1,22 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
+import Image, { type ImageProps } from 'next/image'
+import { Button } from '@repo/ui/button'
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
+type Props = Omit<ImageProps, 'src'> & {
+  srcLight: string
+  srcDark: string
+}
 
 const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
+  const { srcLight, srcDark, ...rest } = props
 
   return (
     <>
       <Image {...rest} src={srcLight} className="imgLight" />
       <Image {...rest} src={srcDark} className="imgDark" />
     </>
-  );
-};
+  )
+}
 
 export default function Home() {
-  return (
-    <></>
-  );
+  return <></>
 }
