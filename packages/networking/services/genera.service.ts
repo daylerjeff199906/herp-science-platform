cb59c8d7f0de5a02b6da34aa44ccd49ee26f4dd4
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-02-06 17:54:50 -0500

    add services taxonomia

---
+++ packages/networking/services/genera.service.ts
@@ -0,0 +1,13 @@
+import { apiClient } from '../client'
+import { GenusResponse, GenusFilter, Genus } from '@repo/shared-types';
+import { ENDPOINTS } from "../config/endpoints-url";
+
+export const fetchGenera = async (params: GenusFilter): Promise<GenusResponse> => {
+    const response = await apiClient.get<GenusResponse>(ENDPOINTS.GENERA.GET_PAGINATED, { params });
+    return response.data;
+}
+
+export const fetchGenusById = async (id: number): Promise<Genus> => {
+    const response = await apiClient.get<Genus>(ENDPOINTS.GENERA.GET_BY_ID(id));
+    return response.data;
+}
