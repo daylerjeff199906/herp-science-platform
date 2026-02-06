784234eb8483a55e598f0d7feafc516868d52aea
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-02-06 18:11:09 -0500

    add hooks taxonomies

--- apps/discovery-portal/app/[locale]/collections/(list)/layout.tsx
+++ apps/discovery-portal/app/[locale]/collections/(list)/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="min-h-screen bg-gray-50/50">
       <div className='bg-gray-900 h-20' />
       <div className="flex flex-col lg:flex-row">
-        <aside className="w-full lg:w-72 lg:fixed lg:top-20 lg:h-[calc(100vh-5rem)] overflow-y-auto p-4 lg:p-6">
+        <aside className="w-full lg:w-80 lg:fixed lg:top-20 lg:h-[calc(100vh-5rem)] overflow-y-auto p-4 lg:p-6">
           <CollectionsFilters />
         </aside>
         <main className="flex-1 lg:ml-72 p-4 lg:p-6">
