52ae306dddc50def81a3929bf8a402b8f12507f0
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-02-08 08:50:17 -0500

    add institution types

---
+++ packages/shared-types/types/institutions/institutions.ts
@@ -0,0 +1,15 @@
+import { PaginatedResponse, BaseEntity } from '../core'
+
+export interface Institution extends BaseEntity {
+    name: string
+    status: number
+}
+
+export type InstitutionsResponse = PaginatedResponse<Institution>
+
+
+export interface InstitutionFilter {
+    name?: string // Filtro por nombre del país (parcial, case-insensitive)
+    page?: number // Número de página para paginación
+    pageSize?: number // Tamaño de página para paginación
+}
