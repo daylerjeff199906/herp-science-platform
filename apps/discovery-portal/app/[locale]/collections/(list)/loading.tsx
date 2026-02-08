791081cc08a239e9acfe5b5be2049005a8682881
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-02-08 11:58:05 -0500

    add pagination component

--- apps/discovery-portal/app/[locale]/collections/(list)/loading.tsx
+++ apps/discovery-portal/app/[locale]/collections/(list)/loading.tsx
@@ -2,7 +2,7 @@ import { CollectionsLoading } from '@/components/collections/CollectionsLoading'
 
 export default function Loading() {
   return (
-    <div className="mx-auto w-full space-y-4 py-6">
+    <div className="mx-auto w-full space-y-4 py-6 px-4">
       <CollectionsLoading />
     </div>
   )
