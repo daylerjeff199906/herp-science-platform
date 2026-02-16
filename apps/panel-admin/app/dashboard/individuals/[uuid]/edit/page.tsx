ee6efe1237da346e73e541713a9bd06d6b8b0cd6
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-02-16 17:30:08 -0500

    fixed errors

--- apps/panel-admin/app/dashboard/individuals/[uuid]/edit/page.tsx
+++ apps/panel-admin/app/dashboard/individuals/[uuid]/edit/page.tsx
@@ -31,32 +31,32 @@ export default async function EditGeneralPage({ params }: PageProps) {
   // Map individual details to form initial values
   const defaultValues = {
     id: individual.id,
-    code: individual.code,
+    code: individual.code || '',
     weight: individual.weight ? Number(individual.weight) : undefined,
     slaughteredWeight: individual.slaughteredWeight ? Number(individual.slaughteredWeight) : undefined,
     svl: individual.svl ? Number(individual.svl) : undefined,
     tailLength: individual.tailLength ? Number(individual.tailLength) : undefined,
     hasEggs: individual.hasEggs,
-    identDate: individual.identDate,
-    identTime: individual.identTime,
-    geneticBarcode: individual.geneticBarcode,
-    depositCodeGenbank: individual.depositCodeGenbank,
-    sexId: individual.sex?.id,
-    activityId: individual.activity?.id,
-    forestId: individual.forestType?.id,
-    museumId: individual.museum?.id,
-    speciesId: individual.species?.id,
-    occurrenceId: individual.ocurrence?.id,
+    identDate: individual.identDate ?? undefined,
+    identTime: individual.identTime ?? undefined,
+    geneticBarcode: individual.geneticBarcode ?? undefined,
+    depositCodeGenbank: individual.depositCodeGenbank ?? undefined,
+    sexId: Number(individual.sex?.id) || 0,
+    activityId: Number(individual.activity?.id) || 0,
+    forestId: Number(individual.forestType?.id) || 0,
+    museumId: Number(individual.museum?.id) || 0,
+    speciesId: Number(individual.species?.id) || 0,
+    occurrenceId: Number(individual.ocurrence?.id) || 0,
     status: individual.status || 1,
   }
 
   return (
     <GeneralInfoForm
       initialData={defaultValues}
-      sexes={sexes}
-      museums={museums}
-      forestTypes={forestTypes}
-      species={species}
+      sexes={sexes?.map(sex => ({ id: Number(sex.id), name: sex.name })) || []}
+      museums={museums?.map(museum => ({ id: Number(museum.id), name: museum.name })) || []}
+      forestTypes={forestTypes?.map(forestType => ({ id: Number(forestType.id), name: forestType.name })) || []}
+      species={species?.map(species => ({ id: Number(species.id), name: species.commonName })) || []}
       activities={[]} // TODO
       occurrences={[]} // TODO
     />
