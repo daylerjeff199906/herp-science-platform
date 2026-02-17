71c605b4dcdacb5a0e61ec3236eec92a00f4ba88
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-02-16 21:05:48 -0500

    add sidebar

--- apps/bio-intranet/tailwind.config.ts
+++ apps/bio-intranet/tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss"
 import sharedConfig from "@repo/ui/tailwind.config"
 
 const config = {
+    darkMode: ['class'],
     content: [
         './app/**/*.{ts,tsx}',
         './components/**/*.{ts,tsx}',
@@ -9,11 +10,26 @@ const config = {
     ],
     presets: [sharedConfig],
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ["var(--font-sans)", "sans-serif"],
-            },
-        },
+    	extend: {
+    		fontFamily: {
+    			sans: [
+    				'var(--font-sans)',
+    				'sans-serif'
+    			]
+    		},
+    		colors: {
+    			sidebar: {
+    				DEFAULT: 'hsl(var(--sidebar-background))',
+    				foreground: 'hsl(var(--sidebar-foreground))',
+    				primary: 'hsl(var(--sidebar-primary))',
+    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+    				accent: 'hsl(var(--sidebar-accent))',
+    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+    				border: 'hsl(var(--sidebar-border))',
+    				ring: 'hsl(var(--sidebar-ring))'
+    			}
+    		}
+    	}
     },
 } satisfies Config
 
