bc66fde705af4982fa69e0a3fce09f70e72865c7
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-03-17 22:10:01 -0500

    fixed brands apps

--- apps/bio-intranet/components/team-switcher.tsx
+++ apps/bio-intranet/components/team-switcher.tsx
@@ -56,7 +56,7 @@ export function TeamSwitcher({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground overflow-hidden">
+              <div className="flex aspect-square size-8 items-center justify-center rounded border text-sidebar-primary-foreground overflow-hidden">
                 {renderLogo(activeTeam.logo, "size-4")}
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
